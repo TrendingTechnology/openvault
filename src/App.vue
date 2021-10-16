14b6fbccdb9302b6cac6f7cc1fa7b3302dc6d401
Ricardo Balk <14904229+ricardobalk@users.noreply.github.com>
2021-10-16 05:37:58 +0200

    Added TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,4 @@ import Home from './views/Home.vue'
   <Home />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./App.css" />
--- src/components/atom/Button.vue
+++ src/components/atom/Button.vue
@@ -3,19 +3,16 @@ import { defineComponent } from 'vue'
 
 const Button = defineComponent({
   props: {
-    src: {
-      required: true,
-      type: String
-    },
-    alt: {
+    className: {
+      type: String,
       required: false,
-      type: String
+      default: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
     }
-  }
+  },
 })
 export default Button;
 </script>
 
 <template>
-  <button type="button"><slot /></button>
+  <button type="button" :class="className"><slot /></button>
 </template>
